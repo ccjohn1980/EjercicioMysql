759690ab0a92cb1d46ca2f376bf8b89f9e826bc6
l11m01 <l11m01@SURCO.com>
2020-02-20 22:15:21 -0500

    este es un ejercicio

---
+++ procedure_02.sql
@@ -0,0 +1,15 @@
+﻿DELIMITER $$
+DROP PROCEDURE IF EXISTS insertar_producto;
+CREATE PROCEDURE insertar_producto (
+	IN nombre VARCHAR(30),
+	IN precio INT
+)
+BEGIN
+	DECLARE id_prod INT;
+	SELECT FLOOR( 1 + RAND( ) *60) INTO id_prod;
+	INSERT INTO producto
+		(id_producto, desc_producto, precio)
+		VALUES
+		(id_prod, nombre, precio);
+END$$
+DELIMITER ;
